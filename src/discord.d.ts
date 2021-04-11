380b97b6ec56cce87687867d013f219456f15d4f
phlgr <philipp@gartz.dev>
2021-04-11 21:54:43 +0200

    🏷️ Update command type with all available options

--- src/discord.d.ts
+++ src/discord.d.ts
@@ -2,6 +2,9 @@ declare module "discord.js" {
   export interface Command {
     name: string;
     description: string;
+    aliases?: string[];
+    usage?: string;
+    cooldown?: number;
     execute: (message: Message, args: string[]) => any; // Can be `Promise<SomeType>` if using async
   }
   export interface Client {
