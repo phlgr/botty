d783150ac95ddb910039171aa41ae2482c727082
phlgr <philipp@gartz.dev>
2021-04-11 18:58:23 +0200

    ✨ Add !help command

--- commands/help.ts
+++ commands/help.ts
@@ -1,1 +1,7 @@
-export default function showHelp() {}
+import { Message } from "discord.js";
+
+export default function showHelp(message: Message) {
+  message.channel.send(
+    "`!w2g <youtube link>` \n Sends you a link to a w2g Room with the video in queue. \n `!clash <name>` \n WIP"
+  );
+}
