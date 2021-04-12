5f11e06bbdd3aeb2de4d66e4d6b08372ef1729c6
phlgr <philipp@gartz.dev>
2021-04-12 11:40:21 +0200

    ♻️ Refactor dynamic import

--- src/bot.ts
+++ src/bot.ts
@@ -15,8 +15,8 @@ const commandFiles = fs
   .filter((file) => file.endsWith(commandFileType));
 
 commandFiles.map((commandFile) => {
-  import(`./commands/${commandFile}`).then((command) =>
-    client.commands.set(command.default.name, command.default)
+  import(`./commands/${commandFile}`).then(({ default: command }) =>
+    client.commands.set(command.name, command)
   );
 });
 
